c1cf6a49e94773ebfbbeaa6974e489bc5fcc28b4
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-01-23 10:51:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+module "vpc" {
+  source = "../../"
+
+  name = local.name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${local.region}a", "${local.region}b"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_ipv6 = false
 
