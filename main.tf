ac07d95914acc5876ff54416b267abb22e9e0e50
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-01-23 22:22:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
-resource "aws_vpc" "vpc" {
-  count = var.enabled ? 1 : 0
-  cidr_block           = var.vpc-cidr
-  instance_tenancy     = var.instance-tenancy
-  enable_dns_support   = var.enable-dns-support
-  enable_dns_hostnames = var.enable-dns-hostnames
+data "aws_ami" "app_ami" {
+  most_recent = true
 
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
-  
 
