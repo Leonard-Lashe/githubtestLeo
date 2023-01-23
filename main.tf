811260dd28c4c2585f99b055ff195008555c8d7a
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-01-23 22:28:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-}
 
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
@@ -15,4 +22,3 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
-
