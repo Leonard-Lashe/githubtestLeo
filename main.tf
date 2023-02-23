dd1f41f744c11b5f56aa94101062b3e1199b99fc
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-02-23 20:57:08 +0200

    added subnet and ec2 instance

--- main.tf
+++ main.tf
@@ -1,3 +1,36 @@
 resource "aws_vpc" "Leo-VPC" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.Leo-VPC.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+subnet_id = aws_subnet.main.id
+  tags = {
+    Name = "HelloWorld"
+  }
+}
