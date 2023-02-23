fddd49fea061024debd1fb19cd9b446ed3eb6052
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-02-23 21:49:20 +0200

    variable for ec2 instance and subnet

--- Variables.tf
+++ Variables.tf
@@ -1,1 +1,9 @@
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
 
+variable "subnet_prefix" {
+  description = "cidr block of subnet"
+  default     = "10.0.1.0/24"
+}
