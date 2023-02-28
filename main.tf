65ec186e55cce5ff7ae1b923f6161804bed60101
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-02-28 09:08:26 +0200

    adding a private vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 resource "aws_vpc" "leo-vpc" {
   cidr_block = "10.0.0.0/16"
 }
+resource "aws_vpc" "leoprivate-vpc" {
+  cidr_block = "10.0.1.0/16"
+}
+
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.leo-vpc.id
   cidr_block = var.subnet_cidr[0]
