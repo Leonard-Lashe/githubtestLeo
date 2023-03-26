302f2d907b6823043af599dc9153491db7c25976
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-03-26 12:55:07 +0300

    sg update

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ resource "aws_default_security_group" "default" {
   }
 
   egress {
-    from_port   = 443
-    to_port     = 443
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
