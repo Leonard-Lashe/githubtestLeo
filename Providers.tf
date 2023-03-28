6ec6f6dee6b278bcf0db4c36e85bf6bc2c5566ef
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-03-28 15:23:23 +0300

    removed arn

--- Providers.tf
+++ Providers.tf
@@ -10,6 +10,5 @@ provider "aws" {
   region       = "us-west-2"
     access_key = "AKIA5S6WSJQSNUZBYA5W"
   secret_key   = "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"
-   arn         = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
