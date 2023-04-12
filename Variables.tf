822566d83e9ab2851b358ec75f1f97ec6697c338
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-04-12 12:18:27 +0300

    remove subnet var

--- Variables.tf
+++ Variables.tf
@@ -3,8 +3,3 @@ variable "instance_type" {
   default     = "t3.micro"
 }
 
-variable "subnet_cidr" {
-  description = "cidr block of subnet"
-  type        = list
-  default     = ["10.0.1.0/24","10.0.2.0/24"]
-}
