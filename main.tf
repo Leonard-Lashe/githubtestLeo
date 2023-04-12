f0691c08803991ea698b5998aae4b8e21357580a
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-04-12 14:30:21 +0300

    private subnet

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   
-  route_table_id  = element(aws_route_table.private[0].id, count.index)
 
   enable_nat_gateway = true
   single_nat_gateway = true
