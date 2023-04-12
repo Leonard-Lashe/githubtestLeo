361303339a8cf63eae0c86dafe6de56cb0c68164
Leonard-Lashe <123253880+Leonard-Lashe@users.noreply.github.com>
2023-04-12 17:15:25 +0300

    load balancer

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-module "vpc" {
+module "blog_vpc" {
   source    = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -20,12 +20,12 @@ module "vpc" {
   }
 }
 
-module "websg-group" {
+module "blog_sg" {
   source    = "terraform-aws-modules/security-group/aws"
   version   = "4.17.1"
-  name      = "websg_new"
+  name      = "blogsg_new"
   
-   vpc_id = module.vpc.public_subnets[0]
+   vpc_id = module.blog_vpc.public_subnets[0]
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -51,14 +51,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-resource "aws_instance" "web2" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   tags = {
@@ -66,3 +59,42 @@ resource "aws_instance" "web2" {
   }
 }
 
+  module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
